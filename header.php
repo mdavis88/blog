6dc029f2808bdabee9e6288846da518b06c1b264
mdavis88 <54687892+mdavis88@users.noreply.github.com>
2021-11-04 17:36:27 -1000

    Add files via upload

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>PHP project 1</title>
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Fruktur&display=swap" rel="stylesheet">
+	<link rel="stylesheet"  href="css/reset.css">
+	<link rel="stylesheet"  href="css/style.css">
+</head>
+<body>
+	<nav>
+		<div class="wrapper">
+		<a href="index.php"> <img src="img/classic.png" name="img" alt="rs logo"></a>
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="discover.php">About</a></li>
+			<li><a href="blog.php">Find blogs</a></li>
+			<li><a href="signUp.php">Sign up</a></li>
+			<li><a href="login.php">Log in</a></li>
+		</ul>
+	</div>
+</nav>
+
+<div class="wrapper">
